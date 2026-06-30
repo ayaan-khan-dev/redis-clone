fda3acc0f3f3b119d00f779b2aa3cb8105e60972
Ayaan Khan <ayaankhan22307@gmail.com>
2026-06-30 13:23:50 -0500

    Added basic commands (SET, GET, PING, DEL)

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class Main {
+    private static final HashMap<String, String> dataStore = new HashMap<>();
     public static void main(String[] args) {
         int port = 6379;
         
@@ -28,15 +30,51 @@ public class Main {
                     String[] commandParts = new String[arraySize];
                     for (int i = 0; i < arraySize; i++) {
                         in.readLine();
-                        String commandLine = in.readLine();
-                        commandParts[i] = commandLine;
+                        commandParts[i] = in.readLine();
                     }
-                    System.out.println("RESP Protocol: " + String.join(" ", commandParts));
-                    System.out.print("Parsed Command Array: [");
-                    for (String cmd : commandParts) {
-                        System.out.print(" \"" + cmd + "\" ");
+
+                    System.out.println("Received command: " + String.join(" ", commandParts));
+
+                    if (commandParts[0].equals("SET")) {
+                        String key = commandParts[1];
+                        String value = commandParts[2];
+                        dataStore.put(key, value);
+                        System.out.println("SET command executed: Key = " + key + ", Value = " + value);
+                        //RESP format for simple string: +OK\r\n
+                        out.print("+OK\r\n");
+                        out.flush();
+                    } else if (commandParts[0].equals("GET")) {
+                        String key = commandParts[1];
+                        String value = dataStore.get(key);
+                        if (value != null) {
+                            //RESP format for bulk string: $<length>\r\n<value>\r\n
+                            out.print("$" + value.length() + "\r\n" + value + "\r\n");
+                        } else {
+                            //RESP format for nil bulk string: $-1\r\n
+                            out.print("$-1\r\n");
+                        }
+                        out.flush();
+                    } else if (commandParts[0].equals("PING")) {
+                        //RESP format for simple string: +PONG\r\n
+                        out.print("+PONG\r\n");
+                        out.flush();
+                    } else if (commandParts[0].equals("DEL")) {
+                        String key = commandParts[1];
+                        if (dataStore.containsKey(key)) {
+                            dataStore.remove(key);
+                            //RESP format for integer: :<number>\r\n
+                            out.print(":1\r\n");
+                        } else {
+                            out.print(":0\r\n");
+                        }
+                        out.flush();
+                    } else {
+                        System.out.println("Unknown command: " + commandParts[0]);
+                        //RESP format for error: -Error message\r\n
+                        out.print("-ERR unknown command '" + commandParts[0] + "'\r\n");
+                        out.flush();
                     }
-                    System.out.println("]");
+                    
                 } else {
                     System.out.println("Received: " + inputLine);
                 }
